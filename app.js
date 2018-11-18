69fa6336b55eaf9751043681ded15fbcb8c2b5ce
Chirag Jain <csjain@ncsu.edu>
2018-11-18 18:19:45 -0500

    Update dependencies

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var express  = require('express');
-var app      = express();
-var fs       = require("fs");
-var morgan   = require("morgan");
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var morgan = require("morgan");
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
 
-var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flags: 'a'});
-app.use(morgan('combined',{stream: accessLogStream}));
+var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {
+  flags: 'a'
+});
+
+app.use(morgan('combined', {
+  stream: accessLogStream
+}));
 
 var stable = 'http://localhost:5000';
 var beta = 'http://localhost:5001';
 
 app.all("/*", function(req, res) {
-    console.log('redirecting to stable');
-    apiProxy.web(req, res, {target: stable});
-});
+  var client_ip = req.connection.remoteAddress;
+  console.log('redirecting to stable');
+  apiProxy.web(req, res, {
+    target: stable
+  });
+  console.log(client_ip);
 
-app.all("/beta/*", function(req, res) {
-    console.log('redirecting to beta');
-    apiProxy.web(req, res, {target: beta});
+  // console.log('redirecting to beta');
+  // apiProxy.web(req, res, {target: beta});
 });
 
 console.log("Starting reverse proxy server");
