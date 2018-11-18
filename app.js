5f9774da1b25c4258f2b6ba2c90d362b32c6b59d
Chirag Jain <csjain@ncsu.edu>
2018-11-18 18:27:12 -0500

    Added code to get the city

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var fs = require("fs");
 var morgan = require("morgan");
 var httpProxy = require('http-proxy');
+var expressip = require('express-ip');
 var apiProxy = httpProxy.createProxyServer();
 
 var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {
@@ -17,12 +18,12 @@ var stable = 'http://localhost:5000';
 var beta = 'http://localhost:5001';
 
 app.all("/*", function(req, res) {
-  var client_ip = req.connection.remoteAddress;
+  const ipInfo = req.ipInfo;
   console.log('redirecting to stable');
   apiProxy.web(req, res, {
     target: stable
   });
-  console.log(client_ip);
+  console.log(ipInfo.city);
 
   // console.log('redirecting to beta');
   // apiProxy.web(req, res, {target: beta});
