9fec88c76ee08407d3a88b785707038dd35f6702
Chirag Jain <csjain@ncsu.edu>
2018-11-18 19:05:47 -0500

    Added random logic

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var app = express();
 var fs = require("fs");
 var morgan = require("morgan");
 var httpProxy = require('http-proxy');
-var expressip = require('express-ip');
 var apiProxy = httpProxy.createProxyServer();
 
 var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {
@@ -18,15 +17,19 @@ var stable = 'http://localhost:5000';
 var beta = 'http://localhost:5001';
 
 app.all("/*", function(req, res) {
-  const ipInfo = req.ipInfo;
-  console.log('redirecting to stable');
-  apiProxy.web(req, res, {
-    target: stable
-  });
-  console.log(ipInfo.city);
-
-  // console.log('redirecting to beta');
-  // apiProxy.web(req, res, {target: beta});
+  var client_ip = req.connection.remoteAddress;
+  var lastChar = client_ip[client_ip.length -1];
+  if (lastChar % 3 == 0) {
+    console.log('redirecting to stable');
+    apiProxy.web(req, res, {
+      target: stable
+    });
+  } else {
+    console.log('redirecting to beta');
+    apiProxy.web(req, res, {
+      target: beta
+    });
+  }
 });
 
 console.log("Starting reverse proxy server");
